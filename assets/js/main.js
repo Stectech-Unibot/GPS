ce6e74334d6bec9164606c4732db511af530e3ea
Iskren.Stanislavov <iskren.jgd@gmail.com>
2015-10-28 14:18:06 +0200

    fix bottom group of FUTURE features

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,7 +110,7 @@ $(document).ready(function(){
         {id:"#signup-button",                   trigger:"#signup-button" , delay:0   , tweenTime:0.5, staggerTime:0, duration:700},
     // FUTURE RELEASE FEATUREs paralax
         {id:"#future-release>div.left>ul>li",   trigger:"#future-release>div.left>ul>li:first-child", delay:0.15, tweenTime:0.4, staggerTime:0.1, duration:500},
-        {id:"#future-release>div.right>ul>li",  trigger:"#future-release>div.left>ul>li:first-child", delay:0.15, tweenTime:0.4, staggerTime:0.1, duration:500},
+        {id:"#future-release>div.right>ul>li",  trigger:"#future-release>div.right>ul>li:first-child", delay:0.15, tweenTime:0.4, staggerTime:0.1, duration:500},
     //REQUEST EARLY ACCESS
         {id:"#request-access>div",              trigger:"#request-access", delay:1 , tweenTime:1, staggerTime:0.25, duration:350},
     ].forEach(function(settings, index){
