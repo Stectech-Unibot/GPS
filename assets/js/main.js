aac8fe5921326be3efc8b795880ee8ae3d94e541
Iskren.Stanislavov <iskren.jgd@gmail.com>
2015-10-28 22:58:07 +0200

    issue #141
    apply mainFunction after all img.svg are replaced

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+var main = function(){
     // $("html,body").animate({scrollTop:0},200);
     $('.circle').click(function(){
         $(this).toggleClass('is-active');
@@ -275,4 +275,53 @@ $(document).ready(function(){
         .setTween(new CircleAnimation(settings).timeLine)
         .addTo(controller);
     });
-})
+};
+$(document).ready(function(){
+    var imagesToEmbed = jQuery('img.svg');
+    var afterN = imagesToEmbed.size();
+    console.log();
+    var wrapAfter = function(){
+        afterN -= 1;
+        if (!afterN){
+            main();   
+        }
+    };
+    imagesToEmbed.each(function(){
+        // <img> with svg source to embedded svgs. http://stackoverflow.com/a/24933495 -->
+
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+
+            wrapAfter();
+        }, 'xml');
+
+    });
+
+});
