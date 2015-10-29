a89e01c75aced62bb2c4d606ee99e0afceeb9007
Iskren.Stanislavov <iskren.jgd@gmail.com>
2015-10-29 21:20:00 +0200

    delay start with 200px

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,14 +127,15 @@ var main = function(){
     [
         // {id:"#MazeWrapper",                     trigger:"#MazeWrapper"   , delay:0.25, tweenTime:0.5, staggerTime:0.3, duration:700},
     // signUp button
-        {id:"#signup-button",                   trigger:"#signup-button" , delay:0   , tweenTime:0.5, staggerTime:0, duration:700},
+        {id:"#signup-button"                 , offset:  0, trigger:"#signup-button" , delay:0   , tweenTime:0.5, staggerTime:0, duration:700},
     // FUTURE RELEASE FEATUREs paralax
-        {id:"#future-release>div.left>ul>li",   trigger:"#future-release>div.left>ul>li:first-child", delay:0.15, tweenTime:0.4, staggerTime:0.1, duration:500},
-        {id:"#future-release>div.right>ul>li",  trigger:"#future-release>div.right>ul>li:first-child", delay:0.15, tweenTime:0.4, staggerTime:0.1, duration:500},
+        {id:"#future-release>div.left>ul>li" , offset:200, trigger:"#future-release>div.left>ul>li:first-child", delay:0.15, tweenTime:0.4, staggerTime:0.1, duration:300},
+        {id:"#future-release>div.right>ul>li", offset:200, trigger:"#future-release>div.right>ul>li:first-child", delay:0.15, tweenTime:0.4, staggerTime:0.1, duration:300},
     ].forEach(function(settings, index){
         var paralaxScene = new ScrollMagic.Scene({
             triggerElement: settings.trigger,
             duration: settings.duration,
+            offset: settings.offset||0,
             triggerHook: "onEnter"})
         .setTween(new TimelineMax().staggerFromTo(
             $(settings.id).get(),
@@ -293,7 +294,6 @@ $(document).ready(function(){
 
     var imagesToEmbed = jQuery('img.svg');
     var afterN = imagesToEmbed.size();
-    console.log();
     var wrapAfter = function(){
         afterN -= 1;
         if (!afterN){
